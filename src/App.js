0c3049e38d53c79b36d8a9eea69b67ab67bad157
heitorgon <heitor.2000goncalves@gmail.com>
2021-07-02 14:37:06 -0300

    primeiro commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import PrimeiroComponente from './components/PrimeiroComponente';
+
+function App() {
+  return (
+    <PrimeiroComponente />
+  );
+}
+
+export default App;
