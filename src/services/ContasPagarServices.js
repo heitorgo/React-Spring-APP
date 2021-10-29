fa5dd19c85e10cc4faeeabef5d6b5cf73cfff385
Heitor Gonçalves <heitor@hypersoft.com.br>
2021-10-29 09:42:12 -0300

    mudança contaspagarservices.js

--- src/services/ContasPagarServices.js
+++ src/services/ContasPagarServices.js
@@ -1,26 +1,26 @@
 import axios from "axios";
 
-const url_base = "http://localhost:8080/contaspagar"
+const url_base = "http://localhost:8080/contaspagar/"
 
 class ContasPagarServices{
     getContaPagar(){
-        return axios.get(url_base+"/all");
+        return axios.get(url_base+"all");
     }
 
     createContaPagar(contapagar){
-        return axios.post(url_base+"/add", contapagar);
+        return axios.post(url_base+"add", contapagar);
     }
 
     getContaPagarById(id_pagar){
-        return axios.get(url_base+"/locate/"+id_pagar);
+        return axios.get(url_base+"locate/"+id_pagar);
     }
 
     editContaPagar(contapagar){
-        return axios.put(url_base+"/update/"+contapagar.id_pagar, contapagar);
+        return axios.put(url_base+"update/"+contapagar.id_pagar, contapagar);
     }
 
     deleteContaPagar(id_pagar){
-        return axios.delete(url_base+"/delete/"+id_pagar);
+        return axios.delete(url_base+"delete/"+id_pagar);
     }
 }
 
